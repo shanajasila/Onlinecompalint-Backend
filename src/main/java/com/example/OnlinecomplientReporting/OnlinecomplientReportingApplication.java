98294d3dfac2820430557cb0ad2d9397beb0c6a1
shanajasila <shanajasilam@gmail.com>
2022-12-09 14:51:39 +0530

    frst

---
+++ src/main/java/com/example/OnlinecomplientReporting/OnlinecomplientReportingApplication.java
@@ -0,0 +1,13 @@
+package com.example.OnlinecomplientReporting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlinecomplientReportingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlinecomplientReportingApplication.class, args);
+	}
+
+}
